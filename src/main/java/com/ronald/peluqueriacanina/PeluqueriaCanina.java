ad47b0b08e7e81f2c6a4f286669b50baa0ec78a8
Ronald <ronaldalmada91@outlook.com>
2023-08-22 19:59:45 -0400

    Creamos el proyecto, añadimos la ventana principal de la app

---
+++ src/main/java/com/ronald/peluqueriacanina/PeluqueriaCanina.java
@@ -0,0 +1,12 @@
+package com.ronald.peluqueriacanina;
+
+/**
+ *
+ * @author Ronald Almada
+ */
+public class PeluqueriaCanina {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
