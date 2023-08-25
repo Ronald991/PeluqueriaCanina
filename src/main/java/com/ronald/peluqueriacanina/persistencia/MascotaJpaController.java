bc079c7b3f1e163b77b27b6b63d1ef6ced1f4ea7
Ronald <ronaldalmada91@outlook.com>
2023-08-25 11:39:46 -0400

    limpieza de clases controladoras

--- src/main/java/com/ronald/peluqueriacanina/persistencia/MascotaJpaController.java
+++ src/main/java/com/ronald/peluqueriacanina/persistencia/MascotaJpaController.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.ronald.peluqueriacanina.persistencia;
 
 import com.ronald.peluqueriacanina.logica.Mascota;
@@ -17,7 +14,7 @@ import javax.persistence.criteria.Root;
 
 /**
  *
- * @author r
+ * @author Ronald Almada
  */
 public class MascotaJpaController implements Serializable {
 
